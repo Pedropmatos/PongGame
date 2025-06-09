4a1885876b4893b901ddde1292676ab9c9f7ea97
Filipe Silva <filipesilva9225@gmail.com>
2025-06-09 11:21:07 -0300

    completando o CollisionHandler, incrementando o segundo jogador, adcionando linhas e a pontuaç

--- core/src/main/java/com/pingpong/pong/utils/Constants.java
+++ core/src/main/java/com/pingpong/pong/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.pingpong.pong.utils;
 
 public class Constants {
-    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_WIDTH = 650;
     public static final int SCREEN_HEIGHT = 480;
     public static final float PADDLE_WIDTH = 20;
     public static final float PADDLE_HEIGHT = 100;
